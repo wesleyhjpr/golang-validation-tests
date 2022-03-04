033bfbed03f346f6a9624f323de68115bd1f9f21
wesleyhjpr <wesleyhjpr@gmail.com>
2022-03-03 21:02:45 -0300

    testing endpoints

--- main_test.go
+++ main_test.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"golang-gin-api-rest/controllers"
+	"golang-gin-api-rest/database"
+	"golang-gin-api-rest/models"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
@@ -11,13 +13,32 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+var ID int
+
 func SetupTestRoutes() *gin.Engine {
+	gin.SetMode(gin.ReleaseMode)
 	routes := gin.Default()
 	return routes
 }
 
+func CreateStudentMock() {
+	student := models.Student{Name: "Student test",
+		CPF: "12345678901", RG: "123456789"}
+	database.DB.Create(&student)
+	ID = int(student.ID)
+}
+
+func DeleteStudentMock() {
+	var student models.Student
+	database.DB.Delete(&student, ID)
+}
+
 func TestCheckStatusCodeOfTheGreeting(t *testing.T) {
 	r := SetupTestRoutes()
+
+	CreateStudentMock()
+	defer DeleteStudentMock()
+
 	r.GET("/:name", controllers.Greeting)
 	req, _ := http.NewRequest("GET", "/wesley", nil)
 	response := httptest.NewRecorder()
@@ -28,4 +49,28 @@ func TestCheckStatusCodeOfTheGreeting(t *testing.T) {
 	responseBody, _ := ioutil.ReadAll(response.Body)
 
 	assert.Equal(t, mockResponse, string(responseBody))
+}
+
+func TestGetAllStudentsHandler(t *testing.T) {
+	database.ConnectToDatabase()
+	r := SetupTestRoutes()
+	r.GET("/students", controllers.GetAllStudents)
+	req, _ := http.NewRequest("GET", "/students", nil)
+	response := httptest.NewRecorder()
+	r.ServeHTTP(response, req)
+	assert.Equal(t, http.StatusOK, response.Code)
+}
+
+func TestGetStudentByCPF(t *testing.T) {
+	database.ConnectToDatabase()
+
+	CreateStudentMock()
+	defer DeleteStudentMock()
+
+	r := SetupTestRoutes()
+	r.GET("/students/cpf/:cpf", controllers.GetAllStudents)
+	req, _ := http.NewRequest("GET", "/students/cpf/12345678901", nil)
+	response := httptest.NewRecorder()
+	r.ServeHTTP(response, req)
+	assert.Equal(t, http.StatusOK, response.Code)
 }
