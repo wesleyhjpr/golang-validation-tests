99dc4d9edfdaf9bf3a9da25a60ec0fdc6514aa90
wesleyhjpr <wesleyhjpr@gmail.com>
2022-03-04 19:26:05 -0300

    add more endpoint tests

--- main_test.go
+++ main_test.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"golang-gin-api-rest/controllers"
 	"golang-gin-api-rest/database"
 	"golang-gin-api-rest/models"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"strconv"
 	"testing"
 
 	"github.com/gin-gonic/gin"
@@ -36,19 +39,16 @@ func DeleteStudentMock() {
 func TestCheckStatusCodeOfTheGreeting(t *testing.T) {
 	r := SetupTestRoutes()
 
-	CreateStudentMock()
-	defer DeleteStudentMock()
-
 	r.GET("/:name", controllers.Greeting)
 	req, _ := http.NewRequest("GET", "/wesley", nil)
 	response := httptest.NewRecorder()
 	r.ServeHTTP(response, req)
 
 	assert.Equal(t, http.StatusOK, response.Code, "should be equal")
-	mockResponse := `{"API says:":"what's up wesley"}`
+	responseMock := `{"API says:":"what's up wesley"}`
 	responseBody, _ := ioutil.ReadAll(response.Body)
 
-	assert.Equal(t, mockResponse, string(responseBody))
+	assert.Equal(t, responseMock, string(responseBody))
 }
 
 func TestGetAllStudentsHandler(t *testing.T) {
@@ -61,16 +61,82 @@ func TestGetAllStudentsHandler(t *testing.T) {
 	assert.Equal(t, http.StatusOK, response.Code)
 }
 
-func TestGetStudentByCPF(t *testing.T) {
+func TestGetStudentByCPFHandler(t *testing.T) {
 	database.ConnectToDatabase()
 
 	CreateStudentMock()
 	defer DeleteStudentMock()
 
 	r := SetupTestRoutes()
-	r.GET("/students/cpf/:cpf", controllers.GetAllStudents)
+	r.GET("/students/cpf/:cpf", controllers.GetStudentByCPF)
 	req, _ := http.NewRequest("GET", "/students/cpf/12345678901", nil)
 	response := httptest.NewRecorder()
 	r.ServeHTTP(response, req)
 	assert.Equal(t, http.StatusOK, response.Code)
+}
+
+func TestGetStudentByIDHandler(t *testing.T) {
+	database.ConnectToDatabase()
+
+	CreateStudentMock()
+	defer DeleteStudentMock()
+
+	r := SetupTestRoutes()
+	r.GET("/students/:id", controllers.GetStudentById)
+	path := "/students/" + strconv.Itoa(ID)
+	req, _ := http.NewRequest("GET", path, nil)
+	response := httptest.NewRecorder()
+	r.ServeHTTP(response, req)
+	var studentMock models.Student
+
+	json.Unmarshal(response.Body.Bytes(), &studentMock)
+
+	assert.Equal(t, "Student test", studentMock.Name, "The name should be equal")
+	assert.Equal(t, "12345678901", studentMock.CPF)
+	assert.Equal(t, "123456789", studentMock.RG)
+	assert.Equal(t, http.StatusOK, response.Code)
+}
+
+func TestDeleteStudentHandler(t *testing.T) {
+	database.ConnectToDatabase()
+
+	CreateStudentMock()
+	defer DeleteStudentMock()
+
+	r := SetupTestRoutes()
+	r.DELETE("/students/:id", controllers.DeleteStudent)
+	path := "/students/" + strconv.Itoa(ID)
+	req, _ := http.NewRequest("DELETE", path, nil)
+	response := httptest.NewRecorder()
+	r.ServeHTTP(response, req)
+
+	assert.Equal(t, http.StatusOK, response.Code)
+}
+
+func TestUpdateStudentHandler(t *testing.T) {
+	database.ConnectToDatabase()
+
+	CreateStudentMock()
+	defer DeleteStudentMock()
+
+	r := SetupTestRoutes()
+	r.PATCH("/students/:id", controllers.UpdateStudent)
+
+	student := models.Student{Name: "Student test",
+		CPF: "99999999999", RG: "888888888"}
+	jsonContent, _ := json.Marshal(student)
+
+	path := "/students/" + strconv.Itoa(ID)
+
+	req, _ := http.NewRequest("PATCH", path, bytes.NewBuffer(jsonContent))
+	response := httptest.NewRecorder()
+	r.ServeHTTP(response, req)
+
+	var studentUpdatedMock models.Student
+
+	json.Unmarshal(response.Body.Bytes(), &studentUpdatedMock)
+
+	assert.Equal(t, "99999999999", studentUpdatedMock.CPF)
+	assert.Equal(t, "888888888", studentUpdatedMock.RG)
+	assert.Equal(t, "Student test", studentUpdatedMock.Name)
 }
